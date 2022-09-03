c7e3811f388b98a12a1f923d9afcea1c33b80c24
lingkai5wu <53172425+lingkai5wu@users.noreply.github.com>
2022-09-03 11:06:27 +0800

    Add files via upload

---
+++ src/daily/No1413.java
@@ -0,0 +1,12 @@
+package daily;
+
+public class No1413 {
+    public int minStartValue(int[] nums) {
+        int sum = 0, min = 0;
+        for (int num : nums) {
+            sum += num;
+            min = Math.min(sum, min);
+        }
+        return 1 - min;
+    }
+}
