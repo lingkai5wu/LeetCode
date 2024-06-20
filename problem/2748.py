321bf5d29436ab2881546362dc8a26fffc7241e4
lingkai5wu <lingkai5wu@outlook.com>
2024-06-20 13:53:32 +0800

    fix: 每日一题

--- problem/2748.py
+++ problem/2748.py
@@ -1,25 +1,33 @@
-from functools import cache
+from math import gcd
 from typing import List
 
 
 class Solution:
     def countBeautifulPairs(self, nums: List[int]) -> int:
-        @cache
-        def gcd(a: int, b: int) -> int:
-            r = a % b
-            if r == 0:
-                return b
-            return gcd(b, r)
+        n = len(nums)
+        res = 0
+        for i in range(n):
+            while nums[i] >= 10:
+                nums[i] //= 10
+            for j in range(i + 1, n):
+                if gcd(nums[i], nums[j] % 10) == 1:
+                    res += 1
+        return res
 
-        count = 0
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if gcd(int(str(nums[i])[0]), int(str(nums[j])[-1])) == 1:
-                    count += 1
-        return count
+    def countBeautifulPairs2(self, nums: List[int]) -> int:
+        count_arr = [0] * 9
+        res = 0
+        for x in nums:
+            for j, count in enumerate(count_arr):
+                if gcd(x % 10, j + 1) == 1:
+                    res += count
+            while x >= 10:
+                x //= 10
+            count_arr[x - 1] += 1
+        return res
 
 
 if __name__ == '__main__':
     nums = [11, 21, 12]
-    res = Solution().countBeautifulPairs(nums)
+    res = Solution().countBeautifulPairs2(nums)
     print(res)
