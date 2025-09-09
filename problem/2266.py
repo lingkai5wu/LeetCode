b59645aa564d0d1370c67c544b1f2df837f425de
lingkai5wu <lingkai5wu@outlook.com>
2025-09-09 11:27:02 +0800

    style: 格式化

--- problem/2266.py
+++ problem/2266.py
@@ -7,6 +7,7 @@ for _ in range(10 ** 5 - 3):  # 预处理所有长度的结果
     f.append((f[-1] + f[-2] + f[-3]) % MOD)
     g.append((g[-1] + g[-2] + g[-3] + g[-4]) % MOD)
 
+
 class Solution:
     def countTexts(self, pressedKeys: str) -> int:
         ans = 1
